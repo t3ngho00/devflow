f45b20c031103bcf984f470ddcc83f491080537c
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2025-09-16 15:06:13 +0300

    Set up Light & Dark Theme, Shadcn, Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
+    <>
     <h1 className="font-space-grotesk">Welcome to DevFlow App</h1>
+    
+    </>
   );
 }
---
+++ context/Theme.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import {
+  ThemeProvider as NextThemeProvider,
+  ThemeProviderProps,
+} from "next-themes";
+import React from "react";
+
+const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+  return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
+};
+
+export default ThemeProvider;
