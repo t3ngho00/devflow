b39022a09f20656f878bcc3f791b65e36c75860d
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2025-09-17 13:13:37 +0300

    Add Google as an authentication provider and update SocialAuthForm for Google sign-in

--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -40,7 +40,7 @@ const SocialAuthForm = () => {
         <span>Log in with GitHub</span>
       </Button>
 
-      <Button className={buttonClass}>
+      <Button className={buttonClass} onClick={() => handleSignIn("google")}>
         <Image
           src="/icons/google.svg"
           alt="Google Logo"
