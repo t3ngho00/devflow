4de41cf36b082f19216864aa201383318c4533ce
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2025-09-30 14:09:22 +0300

    Implement the UI of the Question Form

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,3 +1,17 @@
-export default function AskQuestionPage() {
-  return <div>Ask a Question Page</div>;
-}
+import React from "react";
+
+import QuestionForm from "@/components/forms/QuestionForm";
+
+const AskAQuestion = () => {
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <QuestionForm />
+      </div>
+    </>
+  );
+};
+
+export default AskAQuestion;
--- lib/validation.ts
+++ lib/validation.ts
@@ -11,26 +11,40 @@ export const SignInSchema = z.object({
 });
 
 export const SignUpSchema = z.object({
-    name: z
+  name: z
     .string()
-    .min(1, { message: 'Name is required.' })
-    .max(50, { message: 'Name cannot exceed 50 characters.' })
+    .min(1, { message: "Name is required." })
+    .max(50, { message: "Name cannot exceed 50 characters." })
     .regex(/^[a-zA-Z\s]+$/, {
-      message: 'Name can only contain letters and spaces.',
+      message: "Name can only contain letters and spaces.",
     }),
-    username: z
-        .string()
-        .min(6, { message: "Username must be at least 3 characters long." })
-        .max(30, { message: "Username cannot exceed 30 characters." }),
-    email: z
-        .email("Please provide a valid email address")
-        .min(1, "Email is required"),
-    password: z
-        .string()
-        .min(8, { message: "Password must be at least 8 characters long." })
-        .max(100, { message: "Password cannot exceed 100 characters." })
-        .regex(
-            /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/,
-            "Password must contain at least one letter and one number."
-        ),
+  username: z
+    .string()
+    .min(6, { message: "Username must be at least 3 characters long." })
+    .max(30, { message: "Username cannot exceed 30 characters." }),
+  email: z
+    .email("Please provide a valid email address")
+    .min(1, "Email is required"),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long." })
+    .max(100, { message: "Password cannot exceed 100 characters." })
+    .regex(
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/,
+      "Password must contain at least one letter and one number."
+    ),
+});
+
+export const AskQuestionSchema = z.object({
+  title: z
+    .string()
+    .min(10, { message: "Title must be at least 10 characters long." })
+    .max(100, { message: "Title cannot exceed 150 characters." }),
+  content: z
+    .string()
+    .min(20, { message: "Content must be at least 20 characters long." }),
+  tags: z
+    .array(z.string().min(1, { message: "Tag cannot be empty." }))
+    .min(1, { message: "At least one tag is required." })
+    .max(3, { message: "No more than 3 tags allowed." }),
 });
