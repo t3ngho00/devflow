5ca162eed6f7686af8aff790c842ff81116b31b8
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2025-10-03 13:06:27 +0300

    Implement Answer Model

---
+++ database/answer.model.ts
@@ -0,0 +1,25 @@
+import { Schema, model, Types, models } from "mongoose";
+
+export interface IAnswer {
+  author: Types.ObjectId;
+  content: string;
+  question: Types.ObjectId;
+  upvotes: number;
+  downvotes: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const AnswerSchema = new Schema<IAnswer>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+    content: { type: String, required: true },
+    upvotes: { type: Number, default: 0 },
+    downvotes: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const AnswerModel = models.Answer || model<IAnswer>("Answer", AnswerSchema);
+export default AnswerModel;
--- database/question.model.ts
+++ database/question.model.ts
@@ -11,17 +11,21 @@ export interface IQuestion {
   author: Types.ObjectId;
 }
 
-const QuestionSchema = new Schema<IQuestion>({
-  title: { type: String, required: true },
-  content: {type: String, required: true},
-  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
-  views: { type: Number, default: 0 },
-  answers: { type: Number, default: 0 },
-  upvotes: { type: Number, default: 0 },
-  downvotes: { type: Number, default: 0 },
-  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
-});
+const QuestionSchema = new Schema<IQuestion>(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+    views: { type: Number, default: 0 },
+    answers: { type: Number, default: 0 },
+    upvotes: { type: Number, default: 0 },
+    downvotes: { type: Number, default: 0 },
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true }
+);
 
-const QuestionModel = models.Question || model<IQuestion>("Question", QuestionSchema);
+const QuestionModel =
+  models.Question || model<IQuestion>("Question", QuestionSchema);
 
 export default QuestionModel;
