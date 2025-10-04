afb6e8cf670ba88d7d5572b311c0f02cefb6c1b0
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2025-10-04 17:15:08 +0300

    Implement Interaction Model

---
+++ database/interaction.model.ts
@@ -0,0 +1,31 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IInteraction {
+  user: Types.ObjectId;
+  action: string;
+  actionId: Types.ObjectId;
+  actionType: "Question" | "Answer";
+}
+
+const InteractionSchema = new Schema<IInteraction>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    action: { type: String, required: true },
+    actionId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      refPath: "actionType",
+    },
+    actionType: {
+      type: String,
+      enum: ["Question", "Answer"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Interaction =
+  models.Interaction || model<IInteraction>("Interaction", InteractionSchema);
+
+export default Interaction;
