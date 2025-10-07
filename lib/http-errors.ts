5617edb95931fbf3a78e2ea096e373e0e96fa486
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2025-10-07 14:05:45 +0300

    Implement Error Handling

---
+++ lib/http-errors.ts
@@ -0,0 +1,54 @@
+export class RequestError extends Error {
+  constructor(
+    public statusCode: number,
+    message: string,
+    public errors?: Record<string, string[]>
+  ) {
+    super(message);
+    this.name = "Request Error";
+  }
+}
+
+export class ValidationError extends RequestError {
+  constructor(fieldErrors: Record<string, string[]>) {
+    const message = ValidationError.formatFieldErrors(fieldErrors);
+    super(400, message, fieldErrors);
+    this.name = "ValidationError";
+    this.errors = fieldErrors;
+  }
+
+  static formatFieldErrors(errors: Record<string, string[]>): string {
+    const formattedMessage = Object.entries(errors).map(([field, message]) => {
+      const fieldName = field[0].toUpperCase() + field.slice(1);
+
+      if (message[0] === "Required") {
+        return `${fieldName} is required`;
+      } else {
+        return message.join(" and ");
+      }
+    });
+
+    return formattedMessage.join(", ");
+  }
+}
+
+export class NotFoundError extends RequestError {
+  constructor(resource: string) {
+    super(404, `${resource} not found`);
+    this.name = "NotFoundError";
+  }
+}
+
+export class ForbiddenError extends RequestError {
+  constructor(message: string = "Forbidden") {
+    super(403, message);
+    this.name = "ForbiddenError";
+  }
+}
+
+export class UnauthorizedError extends RequestError {
+  constructor(message: string = "Unauthorized") {
+    super(401, message);
+    this.name = "UnauthorizedError";
+  }
+}
