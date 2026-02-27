49d37f4862346b3413fcd926ada0a6a788843707
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2026-02-27 08:32:00 +0200

    Implement Answer Voting

--- components/cards/AnswerCard.tsx
+++ components/cards/AnswerCard.tsx
@@ -1,12 +1,23 @@
 import { formatDistance } from "date-fns";
 import Link from "next/link";
+import { Suspense } from "react";
 
 import ROUTES from "@/constants/ROUTES";
+import { hasVoted } from "@/lib/actions/vote.action";
 
 import Preview from "../editor/Preview";
 import UserAvatar from "../UserAvatar";
+import Votes from "../votes/Votes";
 
-const AnswerCard = ({ _id, author, content, createdAt }: Answer) => {
+const AnswerCard = ({
+  _id,
+  author,
+  content,
+  createdAt,
+  upvotes,
+  downvotes,
+}: Answer) => {
+  const hasVotedPromise = hasVoted({ targetId: _id, targetType: "answer" });
   return (
     <article className="light-border border-b py-10">
       <span id={JSON.stringify(_id)} className="hash-span" />
@@ -35,7 +46,17 @@ const AnswerCard = ({ _id, author, content, createdAt }: Answer) => {
           </Link>
         </div>
 
-        <div className="flex justify-end">Votes</div>
+        <div className="flex justify-end">
+          <Suspense>
+            <Votes
+              upvotes={upvotes}
+              downvotes={downvotes}
+              hasVotedPromise={hasVotedPromise}
+              targetId={_id}
+              targetType="answer"
+            />
+          </Suspense>
+        </div>
       </div>
 
       <Preview content={content} />
