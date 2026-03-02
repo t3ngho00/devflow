56bf81f196ccbd737b9d491c0aa1bfe6746185d7
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2026-03-02 08:12:05 +0200

    Implement Community Page

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,3 +1,43 @@
-export default function CommunityPage() {
-  return <div>Community Page</div>;
+import DataRenderer from "@/components/DataRenderer";
+import LocalSearch from "@/components/search/LocalSearch";
+import { EMPTY_USERS } from "@/constants/states";
+import { getUsers } from "@/lib/actions/user.action";
+
+export default async function CommunityPage({ searchParams }: RouteParams) {
+  const { page, pageSize, query, sort } = await searchParams;
+  const { success, data, error } = await getUsers({
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query,
+    sort,
+  });
+
+  const { users } = data || {};
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">All Users</h1>
+
+      <div className="mt-11">
+        <LocalSearch
+          imgSrc="/icons/search.svg"
+          placeholder="There are some great devs here!"
+          otherClasses="flex-1"
+        />
+      </div>
+
+      <DataRenderer
+        success={success}
+        error={error}
+        data={users}
+        empty={EMPTY_USERS}
+        render={(users) => (
+          <div className="mt-12 flex flex-wrap gap-5">
+            {users.map((user) => (
+              <p key={user._id}>{user.email}</p>
+            ))}
+          </div>
+        )}
+      />
+    </div>
+  );
 }
--- constants/states.ts
+++ constants/states.ts
@@ -53,3 +53,8 @@ export const EMPTY_ANSWERS = {
   message:
     "The answer board is empty. Make it rain with your brilliant answer.",
 };
+
+export const EMPTY_USERS = {
+  title: "No Users Found",
+  message: "You're ALONE. The only one here, more users coming soon",
+};
